d3a42a38b32ed55fbe979d1facc02cc531fb2094
Mayank Joshi <85271575+git-SwitchBlade@users.noreply.github.com>
2023-04-19 17:40:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
- 
+ const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 4000, fill: "forwards" });
+}
+
+
+
+const menuToggleOpen = document.querySelector("#menu-toggle-open");
+const menuToggleClose = document.querySelector("#menu-toggle-close");
+const menu = document.querySelector(".menu");
+
+const tl = gsap.timeline({
+  paused: true
+});
+
+tl.to(".menu", {
+  x: 0,
+  duration: 0.6,
+  ease: "expo.inOut"
+});
+
+tl.from(".menu nav a", {
+  y: 40,
+  opacity: 0,
+  duration: 0.4,
+  ease: "expo.out",
+  stagger: 0.1
+});
+
+tl.from(".social", {
+  y: 40,
+  opacity: 0,
+  duration: 0.4,
+  ease: "expo.out"
+});
+
+menuToggleOpen.addEventListener("click", () => tl.play());
+menuToggleClose.addEventListener("click", () => tl.reverse());
